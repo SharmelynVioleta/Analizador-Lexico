7d988e63618af502ab36989878e92c4fb8923e34
Sharmelyn Violeta <sbautistal@ulasalle.edu.pe>
2022-06-20 01:40:14 -0500

    Creando los tokens del archivo Lex.flex

---
+++ Tokens.java
@@ -0,0 +1,23 @@
+package Codigo;
+
+public enum Tokens {
+    Palabra_reservada,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    Punto_y_coma,
+    Parentesis,
+    Llaves,
+    Comillas,
+    Comilla_simple,
+    Flecha,
+    Punto,
+    Comentario,
+    Cadena,
+    Dos_Puntos,
+    ERROR
+}
